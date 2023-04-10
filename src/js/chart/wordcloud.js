e9c455136ec8507f6e233082a09640d92f467e3d
재현 김 <kjh9527@esjee.co.kr>
2023-04-10 12:19:46 +0900

    -

--- src/js/chart/wordcloud.js
+++ src/js/chart/wordcloud.js
@@ -1,59 +1,57 @@
-  // Sample data
-  const words = [
-    {text: "드라마", size: 150},
-    {text: "예능", size: 120},
-    {text: "뉴스", size: 75},
-    {text: "스포츠", size: 60},
-    {text: "오늘의 날씨", size: 60},
-    {text: "금융", size: 60},
-    {text: "패션", size: 60},
-    {text: "커피", size: 60},
-    {text: "맥주", size: 60},
-    {text: "음식", size: 55},
-    {text: "유튜브", size: 55},
-    {text: "배달 음식", size: 55},
-    {text: "쿠팡", size: 45},
-    {text: "중고나라", size: 45},
-    {text: "자격증", size: 45},
-    {text: "일자리", size: 45},
-    {text: "전기차", size: 45},
-    {text: "산책로", size: 45},
-    {text: "바람과 구름과 비", size: 35},
-    {text: "신입생 대학 생활", size: 35},
-  ];
-
+function createWordCloud(containerId, words) {
   const wc_width = 800;
   const wc_height = 600;
-
   // Create a word cloud layout
-  const layout = d3.layout.cloud()
-          .size([wc_width, wc_height])
-          .words(words)
-          .padding(5)
-          .rotate(() => ~~(Math.random() * 2) * 90)
-          .fontSize(d => d.size)
-          .on("end", draw);
-
+  const layout = d3
+      .layout
+      .cloud()
+      .size([wc_width, wc_height])
+      .words(words)
+      .padding(5)
+      .rotate(() => ~~ (Math.random() * 2) * 90)
+      .fontSize(d => d.size)
+      .on("end", draw);
   layout.start();
 
-  function draw(words) {
-    // Create an SVG element
-    const svg = d3.select("body").append("svg")
+  // draw 함수는 createWordCloud 함수 내부에 정의되어 있습니다.
+  // 이렇게 함으로써 draw 함수는 createWordCloud 함수의 매개변수와 지역 변수에 접근할 수 있습니다.
+  // 또한, 코드를 구조화하고 재사용하기 쉽게 만듭니다.
+  // 이러한 패턴은 D3.js와 같은 라이브러리에서 종종 사용됩니다.
+  function draw(words) { // Create an SVG element
+    const svg = d3
+        .select(`#${containerId}`)
+        .append("svg")
         .attr("width", wc_width)
         .attr("height", wc_height);
-
-// Add words to the word cloud
-    svg.append("g")
-        .attr("transform", `translate(${wc_width / 2}, ${wc_height / 2})`)
+    // Add words to the word cloud
+    svg
+        .append("g")
+        .attr(
+            "transform",
+            `translate(${
+                wc_width / 2
+            }, ${
+                wc_height / 2
+            })`
+        )
         .selectAll("text")
         .data(words)
-        .enter().append("text")
-        .style("font-size", d => `${d.size}px`)
+        .enter()
+        .append("text")
+        .style("font-size", d => `${
+            d.size
+        }px`)
         .style("fill", (d, i) => d3.schemeCategory10[i % 10])
         .attr("text-anchor", "middle")
-        .attr("transform", d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+        .attr("transform", d => `translate(${
+            [d.x, d.y]
+        })rotate(${
+            d.rotate
+        })`)
         .text(d => d.text)
-        .on("click", d =>
-            console.log(d.srcElement.__data__.text)
-        );
+        .on("click", d => console.log(d
+            .srcElement
+            .__data__
+            .text));
   }
+}
